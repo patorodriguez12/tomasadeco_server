c3e49aa3996676ca1da503fda16ccec326993840
patorodriguez12 <pato_rodriguez12@outlook.com>
2025-02-15 10:28:37 -0300

    Users module, service and controller (for now just returning a message)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, UsersModule],
   controllers: [],
   providers: [],
 })
