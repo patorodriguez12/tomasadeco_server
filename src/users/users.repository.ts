18ad7195b2bdf865d34c06662c00a8679adfdaf3
patorodriguez12 <pato_rodriguez12@outlook.com>
2025-02-24 19:01:43 -0300

    added 2 new properties to users repository

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -5,18 +5,24 @@ export class UsersRepository {
   private readonly users = [
     {
       userId: 1,
+      email: 'V6aFq@example.com',
       username: 'john',
       password: 'changeme',
+      isAdmin: true,
     },
     {
       userId: 2,
+      email: 'cVY6o@example.com',
       username: 'maria',
       password: 'guess',
+      isAdmin: false,
     },
     {
       userId: 3,
+      email: 'cVY6o@example.com',
       username: 'lucy',
       password: 'secret',
+      isAdmin: false,
     },
   ];
 
